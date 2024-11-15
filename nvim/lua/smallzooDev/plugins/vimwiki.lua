d6b662a270c0dbc664215010d103a5b12d36edfc
Joonkyu_Kang <joonkyu_kang@kolon.com>
2024-11-15 17:28:04 +0900

    rollback to leader

--- nvim/lua/smallzooDev/plugins/vimwiki.lua
+++ nvim/lua/smallzooDev/plugins/vimwiki.lua
@@ -4,11 +4,14 @@ return {
 	init = function()
 		vim.g.vimwiki_conceallevel = 0
 		vim.g.vimwiki_global_ext = 0
+		vim.g.vimwiki_folding = ""
 		vim.g.vimwiki_key_mappings = {
 			table_mappings = 0,
 		}
-		vim.keymap.set("n", "<leader>nl", "<Plug>VimwikiNextLink", { silent = true }) -- For Tab
-		vim.keymap.set("n", "<leader>pl", "<Plug>VimwikiPrevLink", { silent = true }) -- For STab:w
+
+		vim.keymap.set("n", "<leader>nl", "<Plug>VimwikiNextLink", { silent = true })
+		vim.keymap.set("n", "<leader>pl", "<Plug>VimwikiPrevLink", { silent = true })
+
 		vim.g.vimwiki_list = {
 			{
 				path = "/Users/jk.kang/wiki/SmallzooDevWiki/content/_wiki",
@@ -20,67 +23,71 @@ return {
 				ext = ".md",
 			},
 		}
+
+		vim.g.vimwiki_syntax = "markdown"
+
+		vim.g.vimwiki_primary_path = vim.g.vimwiki_list[1].path
+		vim.g.vimwiki_secondary_path = vim.g.vimwiki_list[2].path
+
 		vim.cmd([[
-  let g:md_modify_disabled = 0
-  function! NewTemplate ()
-    let l:wiki_directory = v:false
-        
-    for wiki in g:vimwiki_list
-        if expand('%:p:h') =~ expand(wiki.path)
-            let l:wiki_directory= v:true
-            break
+      let g:md_modify_disabled = 0
+
+      function! NewTemplate()
+        " 위키 경로가 설정된 변수로 변경
+        let l:wiki_directory = v:false
+        if expand('%:p:h') =~ expand(g:vimwiki_primary_path) || expand('%:p:h') =~ expand(g:vimwiki_secondary_path)
+          let l:wiki_directory = v:true
         endif
-    endfor
-    if !l:wiki_directory
-        echom 'first debugging point >> called this return statement'
-        return
-    endif
 
-    if line("$") > 1
-        return
-    endif
+        if !l:wiki_directory
+          echom 'first debugging point >> called this return statement'
+          return
+        endif
 
-    let l:template = []
-    call add(l:template, '---')
-    call add(l:template, 'title: ')
-    call add(l:template, 'summary: ')
-    call add(l:template, 'date: ' . strftime('%Y-%m-%d %H:%M:%S +0900'))
-    call add(l:template, 'lastmod: ' . strftime('%Y-%m-%d %H:%M:%S +0900'))
-    call add(l:template, 'tags: ')
-    call add(l:template, 'categories: ')
-    call add(l:template, 'description: ')
-    call add(l:template, 'showToc: true')
-    call add(l:template, 'tocOpen: true')
-    call add(l:template, '---')
-    call add(l:template, '')
-    call add(l:template, '# ')
-    call setline(1, l:template)
-    execute 'normal! G'
-    execute 'normal! $'
+        if line("$") > 1
+          return
+        endif
 
-    echom 'new wiki page has created'
+        let l:template = []
+        call add(l:template, '---')
+        call add(l:template, 'title: ')
+        call add(l:template, 'summary: ')
+        call add(l:template, 'date: ' . strftime('%Y-%m-%d %H:%M:%S +0900'))
+        call add(l:template, 'lastmod: ' . strftime('%Y-%m-%d %H:%M:%S +0900'))
+        call add(l:template, 'tags: ')
+        call add(l:template, 'categories: ')
+        call add(l:template, 'description: ')
+        call add(l:template, 'showToc: true')
+        call add(l:template, 'tocOpen: true')
+        call add(l:template, '---')
+        call add(l:template, '')
+        call add(l:template, '# ')
+        call setline(1, l:template)
+        execute 'normal! G'
+        execute 'normal! $'
 
-  endfunction
+        echom 'new wiki page has created'
+      endfunction
 
-  function! LastModified()
-    if g:md_modify_disabled
-        return
-    endif
-    if &modified
-        " echo('markdown updated time modified')
-        let save_cursor = getpos(".")
-        let n = min([10, line("$")])
-        keepjumps exe '1,' . n . 's#^\(.\{,10}updated\s*: \).*#\1' .
-          \ strftime('%Y-%m-%d %H:%M:%S +0900') . '#e'
-        call histdel('search', -1)
-        call setpos('.', save_cursor)
-    endif
-  endfun
+      function! LastModified()
+        if g:md_modify_disabled
+          return
+        endif
+        if &modified
+          let save_cursor = getpos(".")
+          let n = min([10, line("$")])
+          keepjumps exe '1,' . n . 's#^\(.\{,10}updated\s*: \).*#\1' .
+                \ strftime('%Y-%m-%d %H:%M:%S +0900') . '#e'
+          call histdel('search', -1)
+          call setpos('.', save_cursor)
+        endif
+      endfunction
 
-  augroup wimwikiauto
-    autocmd BufWritePre *wiki/*.md call LastModified()
-    autocmd BufRead,BufNewFile *wiki/*.md call NewTemplate()
-  augroup END
-]])
+      augroup wimwikiauto
+        autocmd!
+        autocmd BufWritePre *wiki/*.md call LastModified()
+        autocmd BufRead,BufNewFile *wiki/*.md call NewTemplate()
+      augroup END
+    ]])
 	end,
 }
