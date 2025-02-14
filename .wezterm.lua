37c388ac1f2ae5f12c2203f1a054d54aeb4d6796
smallzooDev <smallzooDev@gmail.com>
2025-02-14 19:22:08 +0900

    add

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,7 @@ config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 config.font_size = 13
 config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.85
+config.window_background_opacity = 0.75
 config.macos_window_background_blur = 20
 config.enable_tab_bar = false
 -- config.color_scheme = "Catppuccin Mocha"
