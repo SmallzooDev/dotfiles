24efa5be74206902238282563e1dcca5f9641ae5
smallzooDev <smallzooDev@gmail.com>
2025-03-26 18:45:35 +0900

    init go

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,2 @@
--- Rafael Bodill's Neovim entry-point
--- https://github.com/smallzoo/vim-config
---
--- since 2014.
-
-require('smallzoo.config.lazy')
+require("smallzooDev.core")
+require("smallzooDev.lazy")
