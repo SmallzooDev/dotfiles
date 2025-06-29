7ee9af44979444f93c7105adc6010a217965e5c8
SmallzooDev <smallzoodev@gmail.com>
2025-06-29 17:37:25 +0900

    first commit

--- .wezterm.lua
+++ .wezterm.lua
@@ -18,26 +18,26 @@ config.macos_window_background_blur = 20
 
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
-config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Catppuccin Mocha"
 
 -- Catppuccin green cursor
-config.colors = {
-	cursor_bg = "#a6e3a1",
-	cursor_border = "#a6e3a1",
-	cursor_fg = "#1e1e2e",
-}
+-- config.colors = {
+-- 	cursor_bg = "#a6e3a1",
+-- 	cursor_border = "#a6e3a1",
+-- 	cursor_fg = "#1e1e2e",
+-- }
 
 -- coolnight theme
--- config.colors = {
--- foreground = "#CBE0F0",
--- background = "#011423",
--- cursor_bg = "#47FF9C",
--- cursor_border = "#47FF9C",
--- cursor_fg = "#011423",
--- selection_bg = "#033259",
--- selection_fg = "#CBE0F0",
--- ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
--- brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
--- }
+config.colors = {
+foreground = "#CBE0F0",
+background = "#011423",
+cursor_bg = "#47FF9C",
+cursor_border = "#47FF9C",
+cursor_fg = "#011423",
+selection_bg = "#033259",
+selection_fg = "#CBE0F0",
+ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+}
 
 return config
