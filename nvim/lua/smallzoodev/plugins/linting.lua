8b55f32984ef6302ad4bbdc37ea22293849c6bfd
SmallzooDev <smallzoodev@gmail.com>
2025-09-04 10:21:17 +0900

    no golangcilint

--- nvim/lua/smallzoodev/plugins/linting.lua
+++ nvim/lua/smallzoodev/plugins/linting.lua
@@ -6,7 +6,7 @@ return {
 
     lint.linters_by_ft = {
       python = { "pylint" },
-      go = { "golangcilint" },
+      -- go = { "golangcilint" },  -- disabled due to exit code 5 issues
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
