c94ed1df6e3d151e857480b51cc16711beea182c
smallzoodev <smallzoodev@gmail.com>
2025-10-02 14:19:34 +0900

    add mapping

--- nvim/lua/smallzoodev/plugins/obsidian.lua
+++ nvim/lua/smallzoodev/plugins/obsidian.lua
@@ -19,5 +19,6 @@ return {
     { "<leader>on", "<cmd>ObsidianNew<CR>", desc = "New note", ft = "markdown" },
     { "<leader>ot", "<cmd>ObsidianTags<CR>", desc = "Search tags", ft = "markdown" },
     { "<leader>b", "<cmd>ObsidianQuickSwitch<CR>", desc = "Quick switch", ft = "markdown" },
+    { "<leader>/", "<cmd>ObsidianSearch<CR>", desc = "Search notes", ft = "markdown" },
   },
 }
