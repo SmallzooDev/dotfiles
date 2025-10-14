0efb5e1ca4f70e62a4f8a1b7cf6f7be2e270eb2c
smallzoodev <smallzoodev@gmail.com>
2025-10-14 16:19:37 +0900

    new binding for obsidian

--- nvim/lua/smallzoodev/plugins/obsidian.lua
+++ nvim/lua/smallzoodev/plugins/obsidian.lua
@@ -41,6 +41,7 @@ return {
   keys = {
     { "<BS>", "<cmd>ObsidianBacklinks<CR>", desc = "Show backlinks", ft = "markdown" },
     { "<leader>on", "<cmd>ObsidianNew<CR>", desc = "New note", ft = "markdown" },
+    { "<leader>ol", "<cmd>ObsidianLinks<CR>", desc = "New note", ft = "markdown" },
     { "<leader>ot", "<cmd>ObsidianTags<CR>", desc = "Search tags", ft = "markdown" },
     { "<leader>b", "<cmd>ObsidianQuickSwitch<CR>", desc = "Quick switch", ft = "markdown" },
     { "<leader>/", "<cmd>ObsidianSearch<CR>", desc = "Search notes", ft = "markdown" },
