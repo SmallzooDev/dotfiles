d5a498f0d10a915070523f9c67efa3a7ee2de7ae
smallzoodev <smallzoodev@gmail.com>
2025-11-15 10:51:59 +0900

    nvim upgrade

--- nvim/lua/smallzoodev/plugins/lsp/lspconfig.lua
+++ nvim/lua/smallzoodev/plugins/lsp/lspconfig.lua
@@ -2,14 +2,11 @@ return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
+    "saghen/blink.cmp",
     { "antosha417/nvim-lsp-file-operations", config = true },
     { "folke/neodev.nvim", opts = {} },
   },
   config = function()
-    -- import cmp-nvim-lsp plugin
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
     local keymap = vim.keymap -- for conciseness
     vim.api.nvim_create_autocmd("LspAttach", {
       group = vim.api.nvim_create_augroup("UserLspConfig", {}),
@@ -61,7 +58,7 @@ return {
     })
 
     -- used to enable autocompletion (assign to every lsp server config)
-    local capabilities = cmp_nvim_lsp.default_capabilities()
+    local capabilities = require("blink.cmp").get_lsp_capabilities()
 
     vim.diagnostic.config({
       signs = {
