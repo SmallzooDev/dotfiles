46b7416b0d65e9fbe95cd56363d855752919fbde
smallzoodev <smallzoodev@gmail.com>
2025-11-17 11:14:23 +0900

    open in new tab

--- nvim/lua/smallzoodev/plugins/telescope.lua
+++ nvim/lua/smallzoodev/plugins/telescope.lua
@@ -31,6 +31,7 @@ return {
             ["<C-j>"] = actions.move_selection_next, -- move to next result
             ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
             ["<C-t>"] = trouble_telescope.open,
+            ["<CR>"] = actions.select_tab, -- open in new tab on enter
           },
         },
       },
