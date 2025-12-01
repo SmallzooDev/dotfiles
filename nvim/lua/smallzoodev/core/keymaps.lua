fbe4e16e7a179e8d7ace3c072c93c3c7069331d4
smallzoodev <smallzoodev@gmail.com>
2025-12-01 09:54:28 +0900

    hello

--- nvim/lua/smallzoodev/core/keymaps.lua
+++ nvim/lua/smallzoodev/core/keymaps.lua
@@ -10,7 +10,7 @@ keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>ss", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
