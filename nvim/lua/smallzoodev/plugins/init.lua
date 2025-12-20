549abf418a02312a6ae1b99947faf333f8f9c4fc
smallzoodev <smallzoodev@gmail.com>
2025-12-20 12:07:36 +0900

    option to move
    to use unix remove thing

--- nvim/lua/smallzoodev/plugins/init.lua
+++ nvim/lua/smallzoodev/plugins/init.lua
@@ -1,4 +1,12 @@
 return {
   "nvim-lua/plenary.nvim",
-  "christoomey/vim-tmux-navigator",
+  {
+    "christoomey/vim-tmux-navigator",
+    keys = {
+      { "<M-h>", "<cmd>TmuxNavigateLeft<cr>" },
+      { "<M-j>", "<cmd>TmuxNavigateDown<cr>" },
+      { "<M-k>", "<cmd>TmuxNavigateUp<cr>" },
+      { "<M-l>", "<cmd>TmuxNavigateRight<cr>" },
+    },
+  },
 }
