5c26c2e4b94f3ec6c444630b3464c1437ccdef43
smallzoodev <smallzoodev@gmail.com>
2026-01-10 12:40:46 +0900

    do some fixes

--- nvim/lua/smallzoodev/plugins/colorscheme.lua
+++ nvim/lua/smallzoodev/plugins/colorscheme.lua
@@ -4,15 +4,15 @@ return {
   config = function()
     require("tokyonight").setup({
       style = "night",
-      transparent = false,
+      transparent = true,
       terminal_colors = true,
       styles = {
         comments = { italic = true },
         keywords = { italic = true },
         functions = {},
         variables = {},
-        sidebars = "dark",
-        floats = "dark",
+        sidebars = "transparent",
+        floats = "transparent",
       },
       sidebars = { "qf", "help" },
       day_brightness = 0.3,
@@ -27,6 +27,14 @@ return {
         hl.LspReferenceText = { bg = colors.bg_highlight, underline = false }
         hl.LspReferenceRead = { bg = colors.bg_highlight, underline = false }
         hl.LspReferenceWrite = { bg = "#3d4f6d", underline = false }
+
+        -- Full transparency for UI elements
+        hl.SignColumn = { bg = "NONE" }
+        hl.LineNr = { bg = "NONE" }
+        hl.CursorLineNr = { bg = "NONE" }
+        hl.FoldColumn = { bg = "NONE" }
+        hl.NormalFloat = { bg = "NONE" }
+        hl.FloatBorder = { bg = "NONE" }
       end,
     })
 
