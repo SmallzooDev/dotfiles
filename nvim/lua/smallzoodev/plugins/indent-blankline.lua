f6e0edcfbfb345514745fb89a67ca4e795aa1f5d
smallzoodev <smallzoodev@gmail.com>
2026-01-17 16:54:24 +0900

    hello world

--- nvim/lua/smallzoodev/plugins/indent-blankline.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "lukas-reineke/indent-blankline.nvim",
-  enabled = false, -- using mini.indentscope instead
-  event = { "BufReadPre", "BufNewFile" },
-  main = "ibl",
-  opts = {
-    indent = { char = "┊" },
-  },
-}
