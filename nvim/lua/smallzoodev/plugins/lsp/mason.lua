d0c824e0397bdd66f4fe6c10fe3b123dd321aad7
SmallzooDev <smallzoodev@gmail.com>
2026-02-28 17:07:23 +0900

    helloworld

--- nvim/lua/smallzoodev/plugins/lsp/mason.lua
+++ nvim/lua/smallzoodev/plugins/lsp/mason.lua
@@ -1,9 +1,9 @@
 return {
-  "williamboman/mason.nvim",
+  "mason-org/mason.nvim",
   event = "VeryLazy",
   cmd = { "Mason", "MasonInstall", "MasonUpdate" },
   dependencies = {
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
