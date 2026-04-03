9fd18e9ee53e1a98187d0bd3598110a435c0e4df
smallzoodev <smallzoodev@gmail.com>
2026-04-03 22:21:07 +0900

    helloworld

--- nvim/lua/smallzoodev/plugins/neo-tree.lua
+++ nvim/lua/smallzoodev/plugins/neo-tree.lua
@@ -23,6 +23,14 @@ return {
     },
     window = {
       position = "float",
+      popup = {
+        size = function()
+          return {
+            width = math.floor(vim.o.columns * 0.8),
+            height = math.floor(vim.o.lines * 0.8),
+          }
+        end,
+      },
     },
     default_component_configs = {
       indent = {
